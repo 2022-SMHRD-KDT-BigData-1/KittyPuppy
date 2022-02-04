278e4471914bf6a85896bfbcae972c90a127793d
Sangmin Park <otatai781@nate.com>
2022-02-04 17:42:04 +0900

    feedCreateCon, UpdateCon 작성, FeedShow 생성- 상민

--- GitTest/src/main/java/com/kittypuppy/service/FeedUpdateCon.java
+++ GitTest/src/main/java/com/kittypuppy/service/FeedUpdateCon.java
@@ -1,16 +1,50 @@
 package com.kittypuppy.service;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kittypuppy.model.FeedDAO;
+import com.kittypuppy.model.FeedDTO;
+import com.kittypuppy.model.MemberDTO;
 
 public class FeedUpdateCon implements iCommand{
 
+	FeedDTO feed = null;
+	FeedDAO dao = new FeedDAO();
+	MemberDTO member = null;
+	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		// post 방식으로 입력값 넘겨 받음.
+		request.setCharacterEncoding("utf-8");
+		String content = request.getParameter("content");
+		String tag = request.getParameter("tag");
+		String openRange = request.getParameter("openRange");
+		String picAddress = request.getParameter("picAddress");
+		
+		// 세션에서 로그인한 사용자 nick 가져오려고 선언
+		HttpSession session = request.getSession();
+		member = (MemberDTO)session.getAttribute("member");
+		
+		// 수정을 선택한 게시물의 feedno 를 받을 수 있는 feedList
+		//int cnt = dao.feedUpdate(new FeedDTO(null, member.getNick(), picAddress, content, tag, null, null, openRange));
+		
+		if ( cnt > 0 ) {
+			response.sendRedirect("mypage.jsp");
+		}else {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>");
+			out.print("alert('게시글 수정 실패');");
+			out.print("location.href='mypage.jsp';");
+			out.print("</script>");			
+		}
 	}
 
 }
