d5bc61022d65211919a1772c8001e10f81f12f28
Sangmin Park <otatai781@nate.com>
2022-02-05 16:15:29 +0900

    LostAniFilterCon-상민

--- GitTest/src/main/java/com/kittypuppy/service/LostAniFilterCon.java
+++ GitTest/src/main/java/com/kittypuppy/service/LostAniFilterCon.java
@@ -1,16 +1,34 @@
 package com.kittypuppy.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LostAniFilterCon implements iCommand{
+import com.kittypuppy.model.LostAniDAO;
+import com.kittypuppy.model.LostAniDTO;
+
+public class LostAniFilterCon implements iCommand {
+
+	LostAniDAO dao = new LostAniDAO();
+	ArrayList<LostAniDTO> lostList = new ArrayList<LostAniDTO>();
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+		request.setCharacterEncoding("utf-8");
+		String tag = (String) request.getParameter("tag");
+
+		// 일단 검색 가능할 것 같은 내용 넣어둠
+		lostList = dao.lostAniFilter(laType, aniName, upKind, kind, sex, aniSize, isTag, feature, place);
+
+		request.setAttribute("lostList", lostList);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("mainPage.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 }
--- GitTest/src/main/java/com/kittypuppy/service/LostAniUpdateCon.java
+++ GitTest/src/main/java/com/kittypuppy/service/LostAniUpdateCon.java
@@ -1,17 +1,56 @@
-
 package com.kittypuppy.service;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class LostAniUpdateCon implements iCommand{
+import com.kittypuppy.model.LostAniDAO;
+import com.kittypuppy.model.LostAniDTO;
+import com.kittypuppy.model.MemberDTO;
+
+public class LostAniUpdateCon implements iCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+		// post 방식으로 입력값 넘겨 받음.
+		request.setCharacterEncoding("utf-8");
+		String laType = request.getParameter("laType");
+		String aniName = request.getParameter("aniName");
+		String aniPic = request.getParameter("aniPic");
+		String upKind = request.getParameter("upKind");
+		String kind = request.getParameter("kind");
+		String sex = request.getParameter("sex");
+		String color = request.getParameter("color");
+		String aniSize = request.getParameter("aniSize");
+		String isTag = request.getParameter("isTag");
+		String feature = request.getParameter("feature");
+		String content = request.getParameter("content");
+		String place = request.getParameter("place");
+
+		// 쿼리스트링에서 get 방식으로 게시물 번호를 받음
+		int lostNo = Integer.parseInt(request.getParameter("lostNo"));
+
+		// dto에 맞는 순서로 값을 배치
+		LostAniDAO dao = new LostAniDAO();
+		int cnt = dao.lostAniUpdate(new LostAniDTO(lostNo, laType, null, aniName, aniPic, upKind, kind, sex,
+				color, aniSize, isTag, feature, place, null, null));
+
+		if (cnt > 0) {
+			response.sendRedirect("lostpage.jsp");
+		} else {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>");
+			out.print("alert('게시글 수정 실패');");
+			out.print("location.href='lostpage.jsp';");
+			out.print("</script>");
+		}
+
 	}
 
 }
