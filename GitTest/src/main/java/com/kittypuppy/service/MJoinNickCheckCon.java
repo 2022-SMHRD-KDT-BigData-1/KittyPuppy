eec64a5670be5f6bf4752dab9e63abe1d6e2a5cb
smhrd <smhrd@121.147.52.24>
2022-02-14 17:14:10 +0900

    joinID,NICKCon 수정, pom_dependency(ajax)추가 (하라)

--- GitTest/src/main/java/com/kittypuppy/service/MJoinNickCheckCon.java
+++ GitTest/src/main/java/com/kittypuppy/service/MJoinNickCheckCon.java
@@ -15,7 +15,7 @@ public class MJoinNickCheckCon implements iCommand {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
-
+		response.setCharacterEncoding("UTF-8");
 		// 이메일, 비밀번호,전화번호,주소
 		String nick = request.getParameter("nick");
 
@@ -32,6 +32,6 @@ public class MJoinNickCheckCon implements iCommand {
 			System.out.println("사용 가능한 닉네임입니다.");
 
 		}
-		out.write(check + "");
+		out.write(check + ""); //ajax결과 값인 result 가 됨, String으로 값을 내보낼수 있도록 ""로 해준다
 	}
 }
